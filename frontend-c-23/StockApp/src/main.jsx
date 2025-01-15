32d48782fb15b29e8e53bfceb62a7e370831fba5
Diego Bruno <dmbruno61@gmail.com>
2025-01-15 19:26:40 -0300

    frontend

---
+++ frontend-c-23/StockApp/src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Login from './components/Login.jsx'
+import Register from './components/Register.jsx'
+import Home from './components/Home.jsx'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    
+    <Home />
+  </StrictMode>,
+)
